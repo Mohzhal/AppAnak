7901212214598d8924d80fd16f797d076499d4d0
Mohzhal <rizal180204@gmail.com>
2024-07-25 12:49:29 +0700

    deploy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.appanak',
+  appName: 'AppAnak',
+  webDir: 'www'
+};
+
+export default config;
